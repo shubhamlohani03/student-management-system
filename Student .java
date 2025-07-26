8e377393e818313ae89c450775ef51f054934783
shubhamlohani03 <shubhamlohani031@gmail.com>
2025-07-26 19:32:31 +0530

    Create Student .java

---
+++ Student .java
@@ -0,0 +1,14 @@
+public class student {
+  private string name;
+  private int rollNo;
+
+public Student(String name, int rollNo) {
+     this.name = name;
+     this.rollNo = rollNo;
+} 
+
+public void displayInfo() {
+  system.out.println("Name: " + name);
+  system.out.println("Roll NO: " + rollNo);
+}
+}
